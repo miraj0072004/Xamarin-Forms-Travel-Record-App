f24fa3f6b35fdfc1dd676d1e5a143f90ca39da75
miraj0072004@gmail.com <miraj0072004@gmail.com>
2020-06-24 19:26:20 +1000

    Performing Offline database sync

---
+++ XamarinLearning/XamarinLearning/Helpers/AzureAppServiceHelper.cs
@@ -0,0 +1,57 @@
+﻿#define OFFLINE_SYNC_ENABLED
+
+
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.MobileServices.Sync;
+using Microsoft.WindowsAzure.MobileServices;
+
+namespace XamarinLearning.Helpers
+{
+    public class AzureAppServiceHelper
+    {
+        
+
+        public static async Task SyncAsync()
+        { 
+            ReadOnlyCollection<MobileServiceTableOperationError> syncErrors = null;
+
+            try
+            {
+                await App.MobileService.SyncContext.PushAsync();
+
+                await App.postTable.PullAsync("userPosts", "");
+            }
+            catch (MobileServicePushFailedException mspfe)
+            {
+                if (mspfe.PushResult != null)
+                {
+                    syncErrors = mspfe.PushResult.Errors;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            if (syncErrors != null)
+            {
+                foreach (var error in syncErrors)
+                {
+                    if (error.OperationKind == MobileServiceTableOperationKind.Update &&
+                        error.Result != null)
+                    {
+                        await error.CancelAndUpdateItemAsync(error.Result);
+                    }
+                    else
+                    {
+                        await error.CancelAndDiscardItemAsync();
+                    }
+                }
+            }
+        }
+    }
+
+}
